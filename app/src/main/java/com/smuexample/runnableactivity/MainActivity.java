dc86f3a0a8ca75371a83e70a1a7a7245107f67c4
H-H Moon <moonssang031@naver.com>
2021-06-02 14:07:58 +0900

    Multi-threading 1

--- app/src/main/java/com/smuexample/runnableactivity/MainActivity.java
+++ app/src/main/java/com/smuexample/runnableactivity/MainActivity.java
@@ -1,58 +1,80 @@
 package com.smuexample.runnableactivity;
 
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    WorkerThread wt;
     Thread wr;
     boolean running = true;
-    String TAG2 = "THREAD2";
 
+    String TAG = "THREAD";
+    String TAG2 = "THREAD";
+
+    class WorkerThread extends Thread {
+        public void run() {
+            int i = 0;
+            for (i = 0 ; i < 20 && running; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+                Log.v(TAG, "Thread time " + i);
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG, "Now I am in onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         running = true;
+        wt = new WorkerThread();
+        wt.start();
+        Log.v(TAG, "Now I am in onStart");
 
         wr = new Thread(new Runnable() {
             @Override
             public void run() {
-                int i =0;
-                for(i=0; i<20 && running; i++){
+                int i = 0;
+                for(i = 0; i < 20 && running; i++) {
                     try {
                         Thread.sleep(1000);
-                    } catch(InterruptedException e){
+                    } catch (InterruptedException e) {
                     }
-                    Log.v(TAG2, "Runable time=" + i);
+                    Log.v(TAG2, "Runnable time = " + i);
                 }
             }
         });
         wr.start();
-        Log.v(TAG2,"Now I am in onStart");
+        Log.v(TAG2, "Now I am in onStart");
     }
+
     @Override
     protected void onStop() {
         super.onStop();
-        running = false;
-        Log.v(TAG2,"Now I am in onStop");
+        running = true;
+        Log.v(TAG, "Now I am in onStop");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.v(TAG2,"Now I am in onPause");
+        Log.v(TAG, "Now I am in onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.v(TAG2,"Now I am in onResume");
+        Log.v(TAG, "Now I am in onResume");
     }
 }
